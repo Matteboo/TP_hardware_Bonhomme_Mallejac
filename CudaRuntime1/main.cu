7f7b1cbe664bcdf5d65ea5acadea5a48d6daab98
JeanMljc <75736975+JeanMljc@users.noreply.github.com>
2022-01-07 11:06:03 +0100

    Update main.cu

--- CudaRuntime1/main.cu
+++ CudaRuntime1/main.cu
@@ -25,8 +25,8 @@ int main(){
     cudaMatrixMult<<<5,5>>>(d_M1, d_M2, d_Mout, n);
     //clock_t end_gpu = clock();
     cudaMemcpy(Mout_gpu, d_Mout, sizeof(float) * n * p, cudaMemcpyDeviceToHost);
-    double time_spent_cpu = (double) (end_cpu-begin_cpu) / CLOCKS_PER_SEC;
-    double time_spent_gpu = (double) (end_gpu-begin_gpu) / CLOCKS_PER_SEC;
+    //double time_spent_cpu = (double) (end_cpu-begin_cpu) / CLOCKS_PER_SEC;
+    //double time_spent_gpu = (double) (end_gpu-begin_gpu) / CLOCKS_PER_SEC;
     //MatrixPrint(M1, n, p);
     //MatrixPrint(M2, n, p);
     MatrixPrint(Mout_cpu,  n,  p);
